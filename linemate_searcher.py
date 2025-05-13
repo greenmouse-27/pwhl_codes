9240d370533b0e4069384cd2a11b262e28c08c9f
greenmouse-27 <e.mcgee.27@gmail.com>
2025-05-13 15:32:13 -0400

    Update linemate_searcher.py
    made code way shorter (probably not more efficient, but it's easier to read now)

--- linemate_searcher.py
+++ linemate_searcher.py
@@ -1,10 +1,12 @@
 import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as plt
 
-teamname = str(input('What team do you want to look at? I recognize most forms/combinations of team and city names:')).upper()
+teamname = str(input('What team do you want to look at?:')).upper()
 
-if teamname in ['MINNESOTA','MN','MINNESOTA FROST','MIN','FROST','FORST','FRUST']:
+while teamname not in ['MINNESOTA','MN','MINNESOTA FROST','FROST','FORST','FRUST','BOSTON','BOS','FLEET','FREET','BOSTON FLEET','MONTREAL','MTL','VICTOIRE','MONTREAL VICTOIRE','VICTWAR','NEW YORK','NEW YORK SIRENS', 'NY','SIRENS', 'WEE WOOS','OTTAWA','OTT','OTTOWA','CHARGE','OTTAWA CHARGE','CHORGE','TORONTO','TOR','TORONTO SCEPTRES','SCEPTRES','SCEPTERS']:
+    print('Sorry, I do not recognize that team name. Could you try again?')
+    teamname = str(input('What team do you want to look at? Please enter Boston, Minnesota, Montreal, New York, Ottawa, or Toronto:')).upper()
+    
+if teamname in ['MINNESOTA','MN','MINNESOTA FROST','FROST','FORST','FRUST']:
     ds = pd.read_csv('MinnesotaFrost_test.csv')
     print('IN FROST WE FRUST')
 elif teamname in ['BOSTON','BOS','FLEET','FREET','BOSTON FLEET']:
@@ -21,65 +23,45 @@ elif teamname in ['OTTAWA','OTT','OTTOWA','CHARGE','OTTAWA CHARGE','CHORGE']:
     print('GO CHARGE GO!')
 elif teamname in ['TORONTO','TOR','TORONTO SCEPTRES','SCEPTRES','SCEPTERS']:
     ds=pd.read_csv('TorontoSceptres.csv')
-    print('GO T.O.!')
+    print('LET\'S GO T.O.!')
 else:
     print('Sorry, I do not recognize that team name')
     quit
-# print(ds)
 
 player_name = str(input('Enter player last name, with no accents:')).title()
+
 playercount = ds['P1'].where(ds['P1']==player_name).dropna().count() + ds['P2'].where(ds['P2']==player_name).dropna().count() + ds['P3'].where(ds['P3']==player_name).dropna().count()
 print(str(player_name)+' played '+str(playercount)+' times')
 
-# print(ds[ds.applymap(lambda x: x == player_name).any(axis=1)])
+while playercount ==0:
+    print('I don\'t recognize that name - could you check the spelling?')
+    player_name = str(input('Enter player last name only, with no accents:')).title()
+    playercount = ds['P1'].where(ds['P1']==player_name).dropna().count() + ds['P2'].where(ds['P2']==player_name).dropna().count() + ds['P3'].where(ds['P3']==player_name).dropna().count()
+    print(str(player_name)+' played '+str(playercount)+' times')
+    
 
 linemates_counts = {}
 
 linemates=[]
 
-for player in ds['P2'].where(ds['P1']==player_name).dropna().values:
-    if player not in linemates:
-        linemates.append(player)
-        linemates_counts[player]=1
-    else:
-        linemates_counts[player]=linemates_counts[player]+1 
+for position in ['P1','P2','P3']:
+    for mate in ['P1','P2','P3']:
+        if position!=mate:
+            for player in ds[mate].where(ds[position]==player_name).dropna().values:
+                if player not in linemates:
+                    linemates.append(player)
+                    linemates_counts[player]=1
+                else:
+                    linemates_counts[player]=linemates_counts[player]+1
 
-for player in ds['P3'].where(ds['P1']==player_name).dropna().values:
-    if player not in linemates:
-        linemates.append(player)
-        linemates_counts[player]=1
-    else:
-        linemates_counts[player]=linemates_counts[player]+1 
-        
-for player in ds['P1'].where(ds['P2']==player_name).dropna().values:
-    if player not in linemates:
-        linemates.append(player)
-        linemates_counts[player]=1
-    else:
-        linemates_counts[player]=linemates_counts[player]+1 
-        
-for player in ds['P3'].where(ds['P2']==player_name).dropna().values:
-    if player not in linemates:
-        linemates.append(player)
-        linemates_counts[player]=1
-    else:
-        linemates_counts[player]=linemates_counts[player]+1 
-
-for player in ds['P1'].where(ds['P3']==player_name).dropna().values:
-    if player not in linemates:
-        linemates.append(player)
-        linemates_counts[player]=1
-    else:
-        linemates_counts[player]=linemates_counts[player]+1 
-
-for player in ds['P2'].where(ds['P3']==player_name).dropna().values:
-    if player not in linemates:
-        linemates.append(player)
-        linemates_counts[player]=1
-    else:
-        linemates_counts[player]=linemates_counts[player]+1
 
 print(linemates_counts)
+if sum(linemates_counts.values())/playercount>1:
+    linemates_times = sum(linemates_counts.values())/2
+else:
+    linemates_times = sum(linemates_counts.values())
+
+print(str(player_name)+' was (probably) listed without linemates '+str(playercount - linemates_times)+' times')
 
 linemates_counts={}
 linemates=[]
@@ -90,46 +72,16 @@ are_you_done = str(input('Do you want to add a second player? (yes/no):'))
 if are_you_done in ['yes','YES','Yes']:
     player2_name = str(input('Enter second player last name:')).title()
     
-    for player in ds['P3'].where(ds['P1']==player_name).dropna().where(ds['P2']==player2_name).dropna().values:
-        if player not in linemates:
-            linemates.append(player)
-            linemates_counts[player]=1
-        else:
-            linemates_counts[player]=linemates_counts[player]+1
-            
-    for player in ds['P3'].where(ds['P2']==player_name).dropna().where(ds['P1']==player2_name).dropna().values:
-        if player not in linemates:
-            linemates.append(player)
-            linemates_counts[player]=1
-        else:
-            linemates_counts[player]=linemates_counts[player]+1
-    
-    for player in ds['P2'].where(ds['P1']==player_name).dropna().where(ds['P3']==player2_name).dropna().values:
-        if player not in linemates:
-            linemates.append(player)
-            linemates_counts[player]=1
-        else:
-            linemates_counts[player]=linemates_counts[player]+1
-            
-    for player in ds['P2'].where(ds['P3']==player_name).dropna().where(ds['P1']==player2_name).dropna().values:
-        if player not in linemates:
-            linemates.append(player)
-            linemates_counts[player]=1
-        else:
-            linemates_counts[player]=linemates_counts[player]+1
-    
-    for player in ds['P1'].where(ds['P2']==player_name).dropna().where(ds['P3']==player2_name).dropna().values:
-        if player not in linemates:
-            linemates.append(player)
-            linemates_counts[player]=1
-        else:
-            linemates_counts[player]=linemates_counts[player]+1
-            
-    for player in ds['P1'].where(ds['P3']==player_name).dropna().where(ds['P2']==player2_name).dropna().values:
-        if player not in linemates:
-            linemates.append(player)
-            linemates_counts[player]=1
-        else:
-            linemates_counts[player]=linemates_counts[player]+1
-            
+    for position in ['P1','P2','P3']:
+        for mate in ['P1','P2','P3']:
+            for third in ['P1','P2','P3']:
+                if position!=mate and position!=third and mate!=third:
+                    for player in ds[third].where(ds[position]==player_name).dropna().where(ds[mate]==player2_name).dropna().values:
+                        if player not in linemates:
+                            linemates.append(player)
+                            linemates_counts[player]=1
+                        else:
+                            linemates_counts[player]=linemates_counts[player]+1
+                        
+          
     print(linemates_counts)
